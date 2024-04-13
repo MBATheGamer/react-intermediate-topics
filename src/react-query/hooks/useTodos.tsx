190247bfa6c2523b3094eaa80d3a9f4c8c57eb7a
MBATheGamer <mbathegamer@gmail.com>
2024-04-13 04:03:45 +0100

    Create a custom query hook

---
+++ src/react-query/hooks/useTodos.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Todo = {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+};
+
+const useTodos = () => {
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: () =>
+      axios
+        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+        .then(response => response.data),
+  });
+};
+
+export default useTodos;
