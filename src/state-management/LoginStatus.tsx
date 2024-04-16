c562db01e860915608a534083c4a5e6e033a9dd8
MBATheGamer <mbathegamer@gmail.com>
2024-04-16 22:33:28 +0100

    Init LoginStatus

---
+++ src/state-management/LoginStatus.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const LoginStatus = () => {
+  const [user, setUser] = useState("");
+
+  if (user)
+    return (
+      <>
+        <div>
+          <span className="mx-2">{user}</span>
+          <a onClick={() => setUser("")} href="#">
+            Logout
+          </a>
+        </div>
+      </>
+    );
+
+  return (
+    <div>
+      <a onClick={() => setUser("mohamed.benamor")} href="#">
+        Login
+      </a>
+    </div>
+  );
+};
+
+export default LoginStatus;
