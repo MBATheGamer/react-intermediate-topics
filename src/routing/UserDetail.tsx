397b0e430d68cdb67f4b373f514c042c5acd6b6a
MBATheGamer <mbathegamer@gmail.com>
2024-04-29 19:01:24 +0100

    Working with nested routes

--- src/routing/UserDetail.tsx
+++ src/routing/UserDetail.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useParams, useSearchParams } from "react-router-dom";
 
-const UserDetailPage = () => {
+const UserDetail = () => {
   const params = useParams();
   const [searchParams] = useSearchParams();
   const location = useLocation();
@@ -14,4 +14,4 @@ const UserDetailPage = () => {
   );
 };
 
-export default UserDetailPage;
+export default UserDetail;
